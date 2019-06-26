4e5a91dd160612c47f9c96c41660673a3417ca4b
Lukasz Bajsarowicz <lukasz.bajsarowicz@strix.net>
2019-06-27 00:54:10 +0200

    MFTF to MailHog wrapper module: README, License and Composer files.

---
+++ registration.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'M2Coach_MFTFMailHogWrapper',
+    __DIR__
+);
